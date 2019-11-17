05f01b82903680af3c543580c6271d12a1e94054
Digno JR Teogalbo <teogalbo.dj193@hotmail.com>
2019-11-16 18:02:00 -0700

    Randomized the rain better.

--- main.py
+++ main.py
@@ -8,36 +8,53 @@ except ImportError:
     import Tkinter as tk
 
 main = tk.Tk()
-c = Canvas(main, bg='blue', width=500, height=500)
+screen_width = 500 #main.winfo_screenwidth() to make fullscreen
+screen_height = 500 #main.winfo_screenheight()
+c = Canvas(main, bg='blue', width=screen_width, height=screen_height)
 
+refreshRate = 16
+rainWidth = 1
+rainAmount = 500
 rainLength = 15
-gravity = 10
+gravity = 0.5
 drops = []
+dropsFallSpeed = []
 
 def draw_drops(): #function to create a raindrop
-    randomPositionX = random.randint(0, 500)
-    randomPositionY = random.randint(-500, 0)
-    rainDrop = c.create_line(randomPositionX, randomPositionY, randomPositionX, randomPositionY+rainLength, fill='red')
+    randomPositionX = random.randint(0, screen_width)
+    randomPositionY = random.randint(-screen_height-rainLength*5, 0)
+    rainDrop = c.create_line(randomPositionX, randomPositionY, randomPositionX, randomPositionY+rainLength, fill='red', width=rainWidth)
     drops.append(rainDrop)
+    dropsFallSpeed.append(1)
+
+for i in range(rainAmount): #draws raindrops
+    draw_drops()
 
 def move_drops():
-    for drop in drops:
-        x1 = c.coords(drops[drop-1])[0]
-        y1 = c.coords(drops[drop-1])[1]
-        x2 = c.coords(drops[drop-1])[2]
-        y2 = c.coords(drops[drop-1])[3]
+    for drop in range(len(drops)):
+        rainSpeed = random.randint(1, 10)
+        randomPositionY = random.randint(-screen_height-rainLength*5, 0)
 
-        fallSpeed = random.randint(1, 5)
-        if y2 > 515: #if y2 coord of raindrop is outside of visible canvas
-            c.move(drops[drop-1], 0, -515) #move raindrop to the top
-        elif x1 > c.coords(box)[0] and x2 < c.coords(box)[2] and y1 > c.coords(box)[1] and y2 < c.coords(box)[3]: #if x1, x2, y1, and y2 coords are within object
-            c.move(drops[drop-1], 0, -515) #return raindrop to the top
+        x1 = c.coords(drops[drop])[0] #establish coordinates for each raindrop
+        y1 = c.coords(drops[drop])[1]
+        x2 = c.coords(drops[drop])[2]
+        y2 = c.coords(drops[drop])[3]
+
+        #print('index 6, fallspeed = '+str(dropsFallSpeed[5])
+
+        if x1 > c.coords(box)[0] and x2 < c.coords(box)[2] and y1 > c.coords(box)[1] and y2 < c.coords(box)[3]: #if x1, x2, y1, and y2 coords are within object
+            dropsFallSpeed[drop] = rainSpeed
+            c.coords(drops[drop], x1, randomPositionY, x2, randomPositionY+rainLength) #return raindrop to the top
+        elif y2 > screen_height+rainLength: #if y2 coord of raindrop is outside of visible canvas
+            dropsFallSpeed[drop] = rainSpeed
+            c.coords(drops[drop], x1, randomPositionY, x2, randomPositionY+rainLength) #return raindrop to the top
         else: #move the raindrop fallspeed downward
-            c.move(drops[drop-1], 0, fallSpeed)
-    main.after(1, move_drops) #loops move_drops
+            c.move(drops[drop], 0, dropsFallSpeed[drop])
 
-for i in range(500): #draws raindrops
-    draw_drops()
+    for speed in range(len(dropsFallSpeed)): #adds factor of gravity to fallspeed
+        dropsFallSpeed[speed] += gravity
+
+    main.after(refreshRate, move_drops) #loops move_drops
 
 box = c.create_rectangle(125, 125, 250, 250, fill='green') #draws box
 
@@ -58,5 +75,5 @@ main.bind('<Up>', up_callback)
 main.bind('<Down>', down_callback)
 
 c.pack()
-main.after(1, move_drops)
+main.after(refreshRate, move_drops) #loops move_drops
 main.mainloop()
