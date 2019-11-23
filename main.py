ffdde612174a547c4af6782427ed190a13201c2e
Digno JR Teogalbo <teogalbo.dj193@hotmail.com>
2019-11-22 21:28:12 -0700

    Tkinter now represents Kinect data. No rain reset ability.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random, time, threading
-
+import random, time, threading, freenect, numpy
 try:
     from tkinter import *
     import tkinter as tk
@@ -8,29 +7,40 @@ except ImportError:
     import Tkinter as tk
 
 main = tk.Tk()
-screen_width = 500  #main.winfo_screenwidth() to make fullscreen
-screen_height = 500 #main.winfo_screenheight()
+screen_width = 480  #main.winfo_screenwidth() to make fullscreen
+screen_height = 480 #main.winfo_screenheight()
 c = Canvas(main, bg='blue', width=screen_width, height=screen_height)
 
 refreshRate = 16 # per ms to move rain
-resolution = 10 # amount of columns and rows
+resolution = 15 # amount of columns and rows
 rainWidth = 1
 rainAmount = 100
 rainLength = 15
 gravity = 0.5
+
+threshold = 150 # number between 0-255
+
 drops = []
 dropsFallSpeed = []
 graph = []
 activePlots = []
 
-def draw_graph(): # create a resolution x resolution grid
-    for row in range(resolution):
-        for col in range(resolution):
-            box = c.create_rectangle((screen_width/resolution)*row, (screen_height/resolution)*(col), (screen_width/resolution)*(row+1), (screen_height/resolution)*(col+1), fill='', outline='black')
-            graph.append([c.coords(box)[0], c.coords(box)[1], c.coords(box)[2], c.coords(box)[3], box])
+def draw_graph(): # initialize grid
+    global depth
+    global d3
 
-    # for box in range(len(graph)): # write the box id within the box for visualization purposes
-        # label = c.create_text(((graph[box][0]+graph[box][2])/2), ((graph[box][1]+graph[box][3])/2), text=str(graph[box][4]))
+    depth,_ = freenect.sync_get_depth() # get frame from kinect
+    d3 = numpy.dstack((depth, depth, depth)).astype(numpy.uint8) # stack data of frames into multidimensional array organized like (y, x, depth)
+
+    for col in range(resolution):
+        for row in range(resolution):
+            box = c.create_rectangle((screen_width/resolution)*row, (screen_height/resolution)*col, (screen_width/resolution)*(row+1), (screen_height/resolution)*(col+1), fill='', outline='black')
+            x1 = c.coords(box)[0]
+            x2 = c.coords(box)[2]
+            y1 = c.coords(box)[1]
+            y2 = c.coords(box)[3]
+            label = c.create_text(((x1+x2)/2, (y1+y2)/2), text=d3[int((y1+y2)/2)][int((x1+x2)/2)][0])
+            graph.append([x1, x2, y1, y2, box, label])
 
 def draw_drops(): #function to create a raindrop
     randomPositionX = random.randint(0, screen_width)
@@ -43,6 +53,9 @@ for i in range(rainAmount): #draws raindrops
     draw_drops()
 
 def move_drops():
+    depth,_ = freenect.sync_get_depth() # get new frame from kinect
+    d3 = numpy.dstack((depth, depth, depth)).astype(numpy.uint8)
+
     for drop in range(len(drops)):
         rainSpeed = random.randint(1, 10)
         randomPositionY = random.randint(-screen_height-rainLength*5, 0)
@@ -67,43 +80,14 @@ def move_drops():
     for speed in range(len(dropsFallSpeed)): #adds factor of gravity to fallspeed
         dropsFallSpeed[speed] += gravity
 
+    for b in range(len(graph)): #update text and color or boxes and labels according to the data from the kinect
+        c.itemconfig(graph[b][5], text=d3[int(c.coords(graph[b][5])[1])][int(c.coords(graph[b][5])[0])][0])
+        if int(c.itemcget(graph[b][5], 'text')) >= threshold:
+            c.itemconfig(graph[b][4], fill='')
+        else:
+            c.itemconfig(graph[b][4], fill='green')
+
     main.after(refreshRate, move_drops) #loops move_drops
-
-def click(event): # function for mouse click rect change
-    for obj in range(len(graph)):
-        if graph[obj][0] <= event.x <= graph[obj][2] and graph[obj][1] <= event.y <= graph[obj][3]:
-            if c.itemcget(graph[obj][4], 'fill') != '':
-                c.itemconfig(graph[obj][4], fill='')
-                activePlots.remove(graph[obj][4])
-            else:
-                c.itemconfig(graph[obj][4], fill='green')
-                activePlots.append(graph[obj][4])
-
-# def movement(event): # function for mouse hover rect change
-#     def movement(event):
-#         for obj in range(len(graph)):
-#             if graph[obj][0] <= event.x <= graph[obj][2] and graph[obj][1] <= event.y <= graph[obj][3]:
-#                 c.itemconfig(graph[obj][4], fill='green')
-#                 activePlots.remove(graph[obj][4])
-#             else:
-#                 c.itemconfig(graph[obj][4], fill='')
-#                 activePlots.remove(graph[obj][4])
-
-def left_callback(self): #functions to move box
-    c.move(box, -5, 0)
-def right_callback(self):
-    c.move(box, 5, 0)
-def up_callback(self):
-    c.move(box, 0, -5)
-def down_callback(self):
-    c.move(box, 0, 5)
-
-# main.bind('<Motion>', movement)
-main.bind('<Button-1>', click)
-main.bind('<Left>', left_callback) # bind functions to inputs
-main.bind('<Right>', right_callback)
-main.bind('<Up>', up_callback)
-main.bind('<Down>', down_callback)
 
 c.pack()
 draw_graph() # init graph
